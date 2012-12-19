ecb2afb8cb516b90a25e2ea26821a8ae79ebf296
James Dunne <james.jdunne@gmail.com>
2012-12-18 21:08:05 -0600

    Attempting to convert map to quads.

--- MainWindow.cs
+++ MainWindow.cs
@@ -21,6 +21,12 @@ namespace jsdoom
             if (z < 0) return zstr;
             return zstr.Substring(0, z);
         }
+
+        public static int AddReturnIndex<T>(this List<T> list, T value)
+        {
+            list.Add(value);
+            return list.Count - 1;
+        }
     }
 
     class MainWindow : OpenTK.GameWindow
@@ -40,11 +46,18 @@ namespace jsdoom
         Matrix4 matrixProjection, matrixModelview;
         Vector3 centerPos;
         float cameraRotation = 0f;
-        float cameraDistance = 0.5f;
-        private int[] vboIds;
-        private Vector3[] verts;
-        private bool setup;
-        private ushort[] lines;
+        float cameraDistance = 0.45f;
+
+        int[] vboIds;
+        bool setup;
+
+        Vertex[] vertexes;
+        LineDef[] linedefs;
+        SideDef[] sidedefs;
+        Sector[] sectors;
+
+        Quad[] quadIndices;
+        Vector3[] quadVerts;
 
         public MainWindow()
             : base()
@@ -93,11 +106,119 @@ namespace jsdoom
             return -1;
         }
 
-        static void MapCoordToVector3(short x, short y, ref Vector3 vec)
+        struct Vertex
+        {
+            public readonly int X, Y;
+
+            public Vertex(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
+        struct Vertex3
+        {
+            public readonly int X, Y, Z;
+
+            public Vertex3(int x, int y, int z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+
+            public Vertex3(Vertex v, int z)
+                : this(v.X, v.Y, z)
+            {
+            }
+        }
+
+        struct Quad
+        {
+            public readonly int A, B, C, D;
+
+            public Quad(int a, int b, int c, int d)
+            {
+                A = a;
+                B = b;
+                C = c;
+                D = d;
+            }
+        }
+
+        class Sector
+        {
+            public readonly int Floorheight;
+            public readonly int Ceilingheight;
+            public readonly string Floorpic;
+            public readonly string Ceilingpic;
+            public readonly int Lightlevel;
+            public readonly int Special;
+            public readonly int Tag;
+
+            public Sector(int floorheight, int ceilingheight, string floorpic, string ceilingpic, int lightlevel, int special, int tag)
+            {
+                Floorheight = floorheight;
+                Ceilingheight = ceilingheight;
+                Floorpic = floorpic;
+                Ceilingpic = ceilingpic;
+                Lightlevel = lightlevel;
+                Special = special;
+                Tag = tag;
+            }
+        }
+
+        class SideDef
+        {
+            public readonly int Textureoffset;
+            public readonly int Rowoffset;
+            public readonly string Toptexture;
+            public readonly string Bottomtexture;
+            public readonly string Midtexture;
+            public readonly Sector Sector;
+
+            public SideDef(int textureoffset, int rowoffset, string toptexture, string bottomtexture, string midtexture, Sector sector)
+            {
+                Textureoffset = textureoffset;
+                Rowoffset = rowoffset;
+                Toptexture = toptexture;
+                Bottomtexture = bottomtexture;
+                Midtexture = midtexture;
+                Sector = sector;
+            }
+        }
+
+        class LineDef
+        {
+            public readonly int V1, V2;
+            public readonly int Flags;
+            public readonly int Special;
+            public readonly int Tag;
+            public readonly SideDef Side0, Side1;
+
+            public LineDef(int v1, int v2, int flags, int special, int tag, SideDef side0, SideDef side1)
+            {
+                V1 = v1;
+                V2 = v2;
+                Flags = flags;
+                Special = special;
+                Tag = tag;
+                Side0 = side0;
+                Side1 = side1;
+            }
+        }
+
+        static void MapCoordToVector3(int x, int y, int z, out Vector3 vec)
         {
             vec.X = x / 8192f;
+            vec.Y = z / 8192f;
             vec.Z = -y / 8192f;
-            vec.Y = 0.0f;
+        }
+
+        static void MapCoordToVector3(List<Vertex3> verts, int i, out Vector3 vec)
+        {
+            MapCoordToVector3(verts[i].X, verts[i].Y, verts[i].Z, out vec);
         }
 
         async Task Game()
@@ -151,44 +272,18 @@ namespace jsdoom
                 return;
             }
 
-            int ml = FindLump(lumps, "map15");
+            int ml = FindLump(lumps, "map01");
 
             // Read vertices:
             var vertexesLump = lumps[ml + (int)MapLump.VERTEXES];
             int numVertexes = vertexesLump.Size / (2 + 2);
-            verts = new Vector3[numVertexes];
+            vertexes = new Vertex[numVertexes];
             for (int i = 0; i < numVertexes; ++i)
             {
                 short x = BitConverter.ToInt16(vertexesLump.Data, i * 2 * sizeof(Int16));
                 short y = BitConverter.ToInt16(vertexesLump.Data, i * 2 * sizeof(Int16) + sizeof(Int16));
-                MapCoordToVector3(x, y, ref verts[i]);
-            }
 
-            // Read linedefs:
-            var linedefsLump = lumps[ml + (int)MapLump.LINEDEFS];
-
-            const int linedefSize = sizeof(short) * 7;
-            int numLineDefs = linedefsLump.Size / linedefSize;
-
-            lines = new ushort[numLineDefs * 2];
-            for (int i = 0; i < numLineDefs; ++i)
-            {
-                short v1 = BitConverter.ToInt16(linedefsLump.Data, (i * linedefSize) + (sizeof(short) * 0));
-                short v2 = BitConverter.ToInt16(linedefsLump.Data, (i * linedefSize) + (sizeof(short) * 1));
-                short flags = BitConverter.ToInt16(linedefsLump.Data, (i * linedefSize) + (sizeof(short) * 2));
-                // ML_DONTSHOW
-                //if ((flags & 128) == 128) continue;
-
-                short special = BitConverter.ToInt16(linedefsLump.Data, (i * linedefSize) + (sizeof(short) * 3));
-                short tag = BitConverter.ToInt16(linedefsLump.Data, (i * linedefSize) + (sizeof(short) * 4));
-                short sidenum0 = BitConverter.ToInt16(linedefsLump.Data, (i * linedefSize) + (sizeof(short) * 5));
-                short sidenum1 = BitConverter.ToInt16(linedefsLump.Data, (i * linedefSize) + (sizeof(short) * 6));
-
-                unchecked
-                {
-                    lines[i * 2 + 0] = (ushort)v1;
-                    lines[i * 2 + 1] = (ushort)v2;
-                }
+                vertexes[i] = new Vertex(x, y);
             }
 
             // Read sectors:
@@ -197,6 +292,7 @@ namespace jsdoom
             const int sectorSize = (sizeof(short) * 5) + (8 * 2);
             int numSectors = sectorsLump.Size / sectorSize;
 
+            sectors = new Sector[numSectors];
             for (int i = 0; i < numSectors; ++i)
             {
                 short floorheight = BitConverter.ToInt16(sectorsLump.Data, (i * sectorSize) + (sizeof(short) * 0));
@@ -206,6 +302,8 @@ namespace jsdoom
                 short lightlevel = BitConverter.ToInt16(sectorsLump.Data, (i * sectorSize) + 16 + (sizeof(short) * 2));
                 short special = BitConverter.ToInt16(sectorsLump.Data, (i * sectorSize) + 16 + (sizeof(short) * 3));
                 short tag = BitConverter.ToInt16(sectorsLump.Data, (i * sectorSize) + 16 + (sizeof(short) * 4));
+
+                sectors[i] = new Sector(floorheight, ceilingheight, floorpic, ceilingpic, lightlevel, special, tag);
             }
 
             // Read sidedefs:
@@ -214,6 +312,7 @@ namespace jsdoom
             const int sidedefSize = (sizeof(short) * 3) + (8 * 3);
             int numSidedefs = sidedefsLump.Size / sidedefSize;
 
+            sidedefs = new SideDef[numSidedefs];
             for (int i = 0; i < numSidedefs; ++i)
             {
                 short textureoffset = BitConverter.ToInt16(sidedefsLump.Data, (i * sidedefSize) + (sizeof(short) * 0));
@@ -223,7 +322,68 @@ namespace jsdoom
                 string midtexture = Encoding.ASCII.GetString(sidedefsLump.Data, (i * sidedefSize) + 16 + (sizeof(short) * 2), 8).ASCIIZ();
                 // Front sector, towards viewer.
                 short sector = BitConverter.ToInt16(sidedefsLump.Data, (i * sidedefSize) + 24 + (sizeof(short) * 2));
+
+                sidedefs[i] = new SideDef(textureoffset, rowoffset, toptexture, bottomtexture, midtexture, sectors[sector]);
             }
+
+            // Read linedefs:
+            var linedefsLump = lumps[ml + (int)MapLump.LINEDEFS];
+
+            const int linedefSize = sizeof(short) * 7;
+            int numLineDefs = linedefsLump.Size / linedefSize;
+
+            linedefs = new LineDef[numLineDefs];
+
+            var verts = new List<Vertex3>(numVertexes * 6);
+            var quads = new List<Quad>(numLineDefs * 3);
+            for (int i = 0; i < numLineDefs; ++i)
+            {
+                short v1 = BitConverter.ToInt16(linedefsLump.Data, (i * linedefSize) + (sizeof(short) * 0));
+                short v2 = BitConverter.ToInt16(linedefsLump.Data, (i * linedefSize) + (sizeof(short) * 1));
+                short flags = BitConverter.ToInt16(linedefsLump.Data, (i * linedefSize) + (sizeof(short) * 2));
+                short special = BitConverter.ToInt16(linedefsLump.Data, (i * linedefSize) + (sizeof(short) * 3));
+                short tag = BitConverter.ToInt16(linedefsLump.Data, (i * linedefSize) + (sizeof(short) * 4));
+                short sidenum0 = BitConverter.ToInt16(linedefsLump.Data, (i * linedefSize) + (sizeof(short) * 5));
+                short sidenum1 = BitConverter.ToInt16(linedefsLump.Data, (i * linedefSize) + (sizeof(short) * 6));
+
+                // Look up SideDefs:
+                SideDef side0, side1;
+                if (sidenum0 >= 0)
+                {
+                    side0 = sidedefs[sidenum0];
+
+                    if (sidenum1 < 0)
+                    {
+                        // One-sided:
+                        side1 = null;
+
+                        quads.Add(new Quad(
+                            verts.AddReturnIndex(new Vertex3(vertexes[v1], side0.Sector.Floorheight)),
+                            verts.AddReturnIndex(new Vertex3(vertexes[v1], side0.Sector.Ceilingheight)),
+                            verts.AddReturnIndex(new Vertex3(vertexes[v2], side0.Sector.Ceilingheight)),
+                            verts.AddReturnIndex(new Vertex3(vertexes[v2], side0.Sector.Floorheight))
+                        ));
+                    }
+                    else
+                    {
+                        // Two-sided:
+                        side1 = sidedefs[sidenum1];
+                    }
+                }
+                else
+                {
+                    throw new Exception("FAIL");
+                }
+
+                linedefs[i] = new LineDef(v1, v2, flags, special, tag, side0, side1);
+            }
+
+            quadVerts = new Vector3[verts.Count];
+            for (int i = 0; i < verts.Count; ++i)
+            {
+                MapCoordToVector3(verts, i, out quadVerts[i]);
+            }
+            quadIndices = quads.ToArray();
 
             // Read things:
             var thingsLump = lumps[ml + (int)MapLump.THINGS];
@@ -241,10 +401,11 @@ namespace jsdoom
                 // Player #1 start:
                 if (type == 1)
                 {
-                    MapCoordToVector3(x, y, ref centerPos);
+                    MapCoordToVector3(x, y, 0, out centerPos);
                 }
             }
 
+            // Game loop:
             while (true)
             {
                 // Create a new Frame to update:
@@ -290,10 +451,10 @@ namespace jsdoom
                 GL.GenBuffers(2, vboIds);
 
                 GL.BindBuffer(BufferTarget.ArrayBuffer, vboIds[0]);
-                GL.BufferData(BufferTarget.ArrayBuffer, new IntPtr(verts.Length * Vector3.SizeInBytes), verts, BufferUsageHint.StaticDraw);
+                GL.BufferData(BufferTarget.ArrayBuffer, new IntPtr(quadVerts.Length * Vector3.SizeInBytes), quadVerts, BufferUsageHint.StaticDraw);
 
                 GL.BindBuffer(BufferTarget.ElementArrayBuffer, vboIds[1]);
-                GL.BufferData(BufferTarget.ElementArrayBuffer, new IntPtr(lines.Length * sizeof(ushort)), lines, BufferUsageHint.StaticDraw);
+                GL.BufferData(BufferTarget.ElementArrayBuffer, new IntPtr(quadIndices.Length * sizeof(int) * 4), quadIndices, BufferUsageHint.StaticDraw);
 
                 GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
                 GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
@@ -301,13 +462,14 @@ namespace jsdoom
             }
             rendered.Set();
 
+            cameraRotation = (cameraRotation < 360f) ? (cameraRotation + 20f * (float)e.Time) : 0f;
+
             //GL.ClearColor(rendering.bg);
             GL.Enable(EnableCap.DepthTest);
 
             GL.ClearColor(Color4.Black);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
-            cameraRotation = (cameraRotation < 360f) ? (cameraRotation + 20f * (float)e.Time) : 0f;
             //Matrix4.CreateRotationY(cameraRotation, out matrixModelview);
             var eye = centerPos + new Vector3(cameraDistance * (float)Math.Cos(cameraRotation * Math.PI / 180.0), 0.25f, cameraDistance * (float)Math.Sin(cameraRotation * Math.PI / 180.0));
             var up = new Vector3(0f, 1f, 0f);
@@ -320,12 +482,13 @@ namespace jsdoom
             GL.BindBuffer(BufferTarget.ArrayBuffer, vboIds[0]);
             GL.VertexPointer(3, VertexPointerType.Float, 0, 0);
 
-            GL.DrawArrays(BeginMode.Points, 0, verts.Length);
+            GL.DrawArrays(BeginMode.Points, 0, quadVerts.Length);
 
             GL.Color4(Color4.Red);
+            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
 
             GL.BindBuffer(BufferTarget.ElementArrayBuffer, vboIds[1]);
-            GL.DrawElements(BeginMode.Lines, lines.Length, DrawElementsType.UnsignedShort, 0);
+            GL.DrawElements(BeginMode.Quads, quadIndices.Length * 4, DrawElementsType.UnsignedInt, 0);
 
             Context.SwapBuffers();
         }
